84e3b34bc63c7132a776d6e3702f662dbb6fe743
manosatpathy <manosatpathy@gmail.com>
2024-12-01 22:49:43 +0530

    feat: implement displaySearchHistory controller

---
+++ server/controllers/displaySearchHistory.js
@@ -0,0 +1,29 @@
+const { user } = require("../models");
+const { searchHistory } = require("../models");
+
+const displaySearchHistory = async (req, res) => {
+  try {
+    const userId = req.query.userId;
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+    const validUser = await user.findByPk(userId);
+    if (!validUser) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const searchHistories = await searchHistory.findAll({
+      where: { userId },
+      attributes: ["query", "timestamp"],
+    });
+    return res.status(200).json(searchHistories);
+  } catch (err) {
+    return res.status(
+      (500).json({
+        message: "Error displaying search history",
+        error: err.message,
+      })
+    );
+  }
+};
+
+module.exports = displaySearchHistory;
