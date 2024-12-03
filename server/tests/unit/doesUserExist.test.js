349df9eda37871cfa4a530e9c49683d32cc8f903
manosatpathy <manosatpathy@gmail.com>
2024-12-03 23:36:46 +0530

    test: add unit tests for doesUserExist function

---
+++ server/tests/unit/doesUserExist.test.js
@@ -0,0 +1,33 @@
+const doesUserExist = require("../../services/doesUserExist");
+const { user } = require("../../models");
+
+jest.mock("../../models", () => ({
+  user: {
+    findOne: jest.fn(),
+  },
+}));
+
+describe("test doesUserExist service", () => {
+  it("should return true if the user exist", async () => {
+    user.findOne.mockResolvedValue({ id: 1, email: "manosatpathy@gmail.com" });
+    const result = await doesUserExist("manosatpathy@gmail.com");
+    expect(result).toBe(true);
+    expect(user.findOne).toHaveBeenCalledWith({
+      where: { email: "manosatpathy@gmail.com" },
+    });
+  });
+
+  it("should return false if user does not exist", async () => {
+    user.findOne.mockResolvedValue(null);
+    const result = await doesUserExist("nonexistent@gmail.com");
+    expect(result).toBe(false);
+    expect(user.findOne).toHaveBeenCalledWith({
+      where: { email: "nonexistent@gmail.com" },
+    });
+  });
+
+  it("should throw error if a database error occurs", async () => {
+    user.findOne.mockRejectedValue(new Error("DB Error"));
+    await expect(doesUserExist("error@gmail.com")).rejects.toThrow("DB Error");
+  });
+});
