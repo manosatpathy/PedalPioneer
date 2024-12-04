d2bd6fb2b0f7dc1d9468d5cdd7d43dc09cfa2da5
manosatpathy <manosatpathy@gmail.com>
2024-12-04 20:39:52 +0530

    test: add validateTagLimit test in tagValidation file

--- server/tests/unit/validation/tagValidation.test.js
+++ server/tests/unit/validation/tagValidation.test.js
@@ -2,13 +2,23 @@ const {
   validateTagLimit,
   validateTagFormat,
 } = require("../../../validation/tagValidation");
-const {tag} = require("../../../models")
+const { tag } = require("../../../models");
 
-jest.mock("../../../models",()=>{
-    tag.findAll
-})
+jest.mock("../../../models", () => ({
+  tag: {
+    findAll: jest.fn(),
+  },
+}));
 
-
-describe("test validateTagLimit function",()=>{
-    const 
-})
+describe("test tagValidation functions", () => {
+  it("should validate tag limit", async () => {
+    const mockResponse = [{ name: "nature" }, { name: "wildlife" }];
+    tag.findAll.mockResolvedValue(mockResponse);
+    const result = await validateTagLimit(["ocean"], 1);
+    expect(result).toEqual({ valid: true });
+    expect(tag.findAll).toHaveBeenCalledWith({
+      where: { photoId: 1 },
+      attributes: ["name"],
+    });
+  });
+});
